611624e951a6d4a878eea584708bed9d04fa79d7
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-02-17 12:50:39 -0500

    Desarrollo lista de actividades con funcionalidades editar y eliminar actividad

--- contador_calorias/src/components/Form.tsx
+++ contador_calorias/src/components/Form.tsx
@@ -1,21 +1,31 @@
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
+import { v4 as uuidv4} from "uuid"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
-import { ActivityActions } from "../reducers/activityReducer"
+import { ActivityActions, ActivityState } from "../reducers/activityReducer"
 
 type FormProps = {
-    dispatch: Dispatch<ActivityActions>
+    dispatch: Dispatch<ActivityActions>,
+    state: ActivityState
 }
 
-const initialState = {
+const initialState : Activity = {
+    id: uuidv4(),
     category: 1,
     name: '',
     calories: 0
 }
 
-export default function Form({dispatch}: FormProps) {
+export default function Form({dispatch, state}: FormProps) {
 
     const [activity, setActivity] = useState<Activity>(initialState)
+
+    useEffect(() => {
+        if(state.activeId){
+            const selectedActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0]
+            setActivity(selectedActivity)
+        }
+    }, [state.activeId])
  
     const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
         const isNumberField = ['category', 'calories'].includes(e.target.id)
@@ -33,7 +43,10 @@ export default function Form({dispatch}: FormProps) {
     const handelSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         dispatch({ type: 'save-activity', payload: {newActivity: activity}})
-        setActivity(initialState)
+        setActivity({
+            ...initialState,
+            id: uuidv4()
+        })
     }
 
   return (
