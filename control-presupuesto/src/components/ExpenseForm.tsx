6e0062f0c5e3cc870195abb80fb383a98e9ab6f3
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-02-21 14:53:40 -0500

    Manejo de estado y validación formulario

--- control-presupuesto/src/components/ExpenseForm.tsx
+++ control-presupuesto/src/components/ExpenseForm.tsx
@@ -1,9 +1,11 @@
-import { useState } from "react";
-import type { DraftExpense } from "../types";
+import { ChangeEvent, FormEvent, useState } from "react";
+import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css'
 import 'react-date-picker/dist/DatePicker.css'
+import ErrorMessage from "./ErrorMessage";
+import { useBudget } from "../hooks/useBudget";
 
 export default function ExpenseForm() {
     const [expense, setExpense] = useState<DraftExpense>({
@@ -12,32 +14,69 @@ export default function ExpenseForm() {
         category: '',
         date: new Date()
     })
+    
+    const [error, setError] = useState('')
+    const {dispatch} = useBudget()
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        const {name, value} = e.target
+        const isAmoundField = ['amount'].includes(name)
+        setExpense({
+            ...expense,
+            [name] : isAmoundField ? +value : value
+        })
+    }
+
+    const handleChangeDate = (value : Value) => {
+        setExpense({
+            ...expense,
+            date: value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        if(Object.values(expense).includes('')) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        dispatch({type: 'add-expense', payload: {expense}})
+        setExpense({
+            amount: 0,
+            expenseName: '',
+            category: '',
+            date: new Date()
+        })
+    }
 
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
         <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
             Nuevo Gasto
         </legend>
+
+        {error && <ErrorMessage>{error}</ErrorMessage>}
 
         <div className="flex flex-col gap-2">
             <label htmlFor="expenseName" className="text-xl">
                 Nombre Gasto:
             </label>
-            <input type="text" name="expenseName" id="expenseName" placeholder="Añade el nombre del gasto" className="bg-slate-100 p-2" value={expense.expenseName}/>
+            <input type="text" name="expenseName" id="expenseName" placeholder="Añade el nombre del gasto" className="bg-slate-100 p-2" value={expense.expenseName} onChange={handleChange}/>
         </div>
 
         <div className="flex flex-col gap-2">
             <label htmlFor="amount" className="text-xl">
                 Cantidad:
             </label>
-            <input type="number" name="amount" id="amount" placeholder="Añade la cantidad del gasto: ej. 300" className="bg-slate-100 p-2" value={expense.amount}/>
+            <input type="number" name="amount" id="amount" placeholder="Añade la cantidad del gasto: ej. 300" className="bg-slate-100 p-2" value={expense.amount} onChange={handleChange}/>
         </div>
 
         <div className="flex flex-col gap-2">
             <label htmlFor="category" className="text-xl">
                 Categoría:
             </label>
-            <select name="category" id="category" className="bg-slate-100 p-2" value={expense.category}>
+            <select name="category" id="category" className="bg-slate-100 p-2" value={expense.category} onChange={handleChange}>
                 <option value="">-- Seleccione --</option>
                 {categories.map( category => (
                     <option key={category.id} value={category.id}>
@@ -54,6 +93,7 @@ export default function ExpenseForm() {
             <DatePicker
                 className="bg-slate-100 p-2 border-0"
                 value={expense.date}
+                onChange={handleChangeDate}
             />
         </div>
 
