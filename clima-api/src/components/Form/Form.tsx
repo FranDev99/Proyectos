6a9d8e24bd4e4509e3b333105b32e39612c61ed3
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-03-02 16:48:13 -0500

    Estilos del formulario inicial con su state y función de cambio

---
+++ clima-api/src/components/Form/Form.tsx
@@ -0,0 +1,54 @@
+import { ChangeEvent, useState } from "react";
+import { countries } from "../../data/countries";
+import type { SearchType } from "../../types";
+import styles from './Form.module.css'
+
+export default function Form() {
+
+    const [search, setSearch] = useState<SearchType>({
+        city: '', 
+        country: ''
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+  return (
+    <form className={styles.form}>
+        <div className={styles.field}>
+            <label htmlFor="city">Ciudad:</label>
+            <input
+                id="city"
+                type="text"
+                name="city"
+                placeholder="Ciudad"
+                value={search.city}
+                onChange={handleChange}
+            />
+        </div>
+        <div className={styles.field}>
+            <label htmlFor="country">País:</label>
+            <select
+                id="country"
+                name="country"
+                value={search.country}
+                onChange={handleChange}
+            >
+                <option>-- Seleccione un país --</option>
+                {countries.map( country => (
+                    <option
+                        key={country.code}
+                        value={country.code}
+                    >{country.name}</option>
+                ))}
+            </select>
+        </div>
+
+        <input className={styles.submit} type="submit" value='Consultar Clima'/>
+    </form>
+  )
+}
