54fa7cc1b12b273c63a7c92d70a8329ec100e0bf
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-03-05 17:44:43 -0500

    Agregar gestión de selección de criptomonedas en el formulario de búsqueda y validar campos obligatorios

--- cripto-api/src/components/CriptoSearchForm.tsx
+++ cripto-api/src/components/CriptoSearchForm.tsx
@@ -1,17 +1,48 @@
 import { useCryptoStore } from "../store"
 import { currencies } from "../data"
+import { ChangeEvent, Children, FormEvent, useState } from "react"
+import { Pair } from "../types"
+import { object, set } from "zod"
+import ErrorMessage from "./ErrorMessage"
 
 export default function CriptoSearchForm() {
 
-    const cryptocurrencies = useCryptoStore(state => state.cryptocurrencies)
+    const cryptocurrencies = useCryptoStore((state) => state.cryptocurrencies)
+    const [pair, setPair] = useState<Pair>({
+        currency: '',
+        criptocurrency: ''
+    })
+
+    const [error, setError] = useState('')
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setPair({
+            ...pair,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if(Object.values(pair).includes('')) {
+            setError('Todos los campos son obligatorios')
+            return
+        }
+        setError('')
+
+    }
+
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={ handleSubmit}>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
         <div className="field">
             <label htmlFor="currency">Moneda:</label>
             <select 
                 name="currency" 
-                id="currency">
+                id="currency"
+                onChange={handleChange}
+                value={pair.currency}>
                     <option value="">-- Seleccione --</option>
                     {currencies.map( currency =>(
                         <option key={currency.code} value={currency.code}>{currency.name}</option>
@@ -23,7 +54,9 @@ export default function CriptoSearchForm() {
             <label htmlFor="criptocurrency">Criptomoneda:</label>
             <select 
                 name="criptocurrency" 
-                id="criptocurrency">
+                id="criptocurrency"
+                onChange={handleChange}
+                value={pair.criptocurrency}>
                     <option value="">-- Seleccione --</option>
                     {cryptocurrencies.map( crypto => (
                         <option key={crypto.CoinInfo.FullName} value={crypto.CoinInfo.Name}>{crypto.CoinInfo.FullName}</option>
