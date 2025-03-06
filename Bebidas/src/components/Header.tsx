8f14afd7c62eec89d44be42907e66cfa0b435d16
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-03-06 14:27:41 -0500

    Agregar estructura básica del proyecto de bebidas con rutas y componentes de página

---
+++ Bebidas/src/components/Header.tsx
@@ -0,0 +1,49 @@
+import { useMemo } from 'react'
+import { NavLink, useLocation } from 'react-router-dom'
+
+export default function Header() {
+
+    const {pathname} = useLocation()
+    const isHome = useMemo(() => pathname === '/',[pathname])
+
+  return (
+    <header className={ isHome ? 'bg-[url(/bg.jpg)]' : 'bg-slate-800'}>
+        <div className="mx-auto container px-5 py-16">
+            <div className="flex justify-between items-center">
+                <div>
+                    <img src="/logo.svg" alt="logotipo" className="w-32"/>
+                </div>
+                <nav className='flex gap-4'>
+                    <NavLink to="/" className={({isActive}) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'}>Inicio</NavLink>
+                    <NavLink to="/favoritos" className={({isActive}) => isActive ? 'text-orange-500 uppercase font-bold' : 'text-white uppercase font-bold'}>Favoritos</NavLink>
+                </nav>
+            </div>
+            {isHome && (
+                <form className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'>
+                    <div className='space-y-4'>
+                        <label htmlFor="ingredient" className='block text-white uppercase font-extrabold text-lg'>Nombres o Ingredientes</label>
+                        <input
+                            id='ingredient'
+                            type='text'
+                            name='ingredient'
+                            className='p-3 w-full rounded-lg focus:outline-none bg-white'
+                            placeholder='Buscar por nombre o ingrediente'
+                        />
+                    </div>
+                    <div className='space-y-4'>
+                        <label htmlFor="category" className='block text-white uppercase font-extrabold text-lg'>Categoría</label>
+                        <select
+                            id='category'
+                            name='category'
+                            className='p-3 w-full rounded-lg focus:outline-none bg-white'
+                        >
+                            <option value="">-- Seleccione --</option>
+                        </select>
+                    </div>
+                    <input type="submit" value='Buscar Recetas' className='cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase'/>
+                </form>
+            )}
+        </div>
+    </header>
+  )
+}
