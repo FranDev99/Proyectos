e660b645bfcb9d09269a416553d24ca71295bc96
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-03-07 17:08:42 -0500

    Agregar gestión de recetas favoritas y modal de visualización de recetas

--- Bebidas/src/stores/recipeSlice.ts
+++ Bebidas/src/stores/recipeSlice.ts
@@ -1,12 +1,16 @@
 import { StateCreator } from "zustand"
-import { getCategories, getRecipes } from "../services/RecipeService"
-import type {Categories, Drinks, SearchFilter} from '../types'
+import { getCategories, getRecipes, getRecipeById } from "../services/RecipeService"
+import type {Categories, Drink, Drinks, Recipe, SearchFilter} from '../types'
 
 export type RecipesSliceType = {
     categories: Categories
     drinks: Drinks
+    selectedRecipe: Recipe
+    modal: boolean
     fetchCategories: () => Promise<void>
     searchRecipes: (searchFilters : SearchFilter) => Promise<void>
+    selectRecipe: (id: Drink['idDrink']) => Promise<void>
+    closeModal: () => void
 }
 
 export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
@@ -16,6 +20,8 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
     drinks: {
         drinks: []
     },
+    selectedRecipe: {} as Recipe,
+    modal: false,
     fetchCategories: async () => {
        const categories = await getCategories()
        set({categories})
@@ -23,5 +29,12 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
     searchRecipes: async (filters) => {
         const drinks = await getRecipes(filters)
         set({drinks})  
+    },
+    selectRecipe: async (id) => {
+        const selectedRecipe = await getRecipeById(id)
+        set({selectedRecipe, modal: true})
+    },
+    closeModal: () => {
+        set({modal: false, selectedRecipe: {} as Recipe})
     }
 }) 
--- Bebidas/src/types/index.ts
+++ Bebidas/src/types/index.ts
@@ -1,7 +1,8 @@
 import {z} from 'zod'
-import { categoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, SearchFilterSchema } from '../schemas/recipes-schema'
+import { categoriesAPIResponseSchema, DrinkAPIResponse, DrinksAPIResponse, RecipeAPIResponseSchema, SearchFilterSchema } from '../schemas/recipes-schema'
 
 export type Categories = z.infer<typeof categoriesAPIResponseSchema>
 export type SearchFilter = z.infer<typeof SearchFilterSchema>
 export type Drinks = z.infer<typeof DrinksAPIResponse>
 export type Drink = z.infer<typeof DrinkAPIResponse>
+export type Recipe = z.infer<typeof RecipeAPIResponseSchema>
